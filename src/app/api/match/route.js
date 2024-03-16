88245f97f69be3f9c07673316de5f89e0c38331b
Ralph Vargas <ralphvargassorm@gmail.com>
2024-03-16 11:20:16 -0400

    increment users total win/loss/points

--- src/app/api/match/route.js
+++ src/app/api/match/route.js
@@ -4,6 +4,9 @@ import prisma from "@/utils/db";
 export const POST = async (request) => {
   const data = await request.json();
 
+  const loserId =
+    data.playerOne === data.winner ? data.playerTwo : data.playerOne;
+
   const newMatch = await prisma.matches.create({
     data: {
       playerOne: data.playerOne,
@@ -12,6 +15,39 @@ export const POST = async (request) => {
       playerTwoUsername: data.playerTwoUsername,
       matchType: parseInt(data.matchType),
       winnerId: data.winner,
+      loserId: loserId,
+    },
+  });
+
+  let pointsToAdd = 0;
+  switch (parseInt(data.matchType)) {
+    case 1:
+      pointsToAdd = 5;
+      break;
+    case 3:
+      pointsToAdd = 10;
+      break;
+    case 5:
+      pointsToAdd = 20;
+      break;
+    default:
+      console.log("Invalid match type for points increment.");
+  }
+
+  console.log(pointsToAdd);
+
+  await prisma.user.update({
+    where: { userId: data.winner },
+    data: {
+      totalPoints: { increment: pointsToAdd },
+      totalWins: { increment: 1 },
+    },
+  });
+
+  await prisma.user.update({
+    where: { userId: loserId },
+    data: {
+      totalLosses: { increment: 1 },
     },
   });
 
