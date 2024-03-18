d39b9ac2d8226e68ac24ff543efe80a87ba593f2
Ralph Vargas <ralphvargassorm@gmail.com>
2024-03-17 22:51:25 -0400

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,9 +17,16 @@ export const getAllUsers = async () => {
 
 export const getRankings = async () => {
   const res = await fetch(
-    new Request(createUrl(`/api/users/rankings`), {
-      method: "GET",
-    })
+    new Request(
+      createUrl(`/api/users/rankings`, {
+        next: {
+          revalidate: 0,
+        },
+      }),
+      {
+        method: "GET",
+      }
+    )
   );
 
   if (res.ok) {
