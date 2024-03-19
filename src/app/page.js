9ecf9adca6a6cdc5c5d453e728ce6da4bacdc08e
Ralph Vargas <ralphvargassorm@gmail.com>
2024-03-18 21:45:27 -0400

    fixed href for get started button

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,18 @@
 import styles from "./page.module.css";
 import Link from "next/link";
 import { Button } from "antd";
+import { auth } from "@clerk/nextjs";
 
 const Home = async () => {
+  const { userId } = await auth();
+  let href = userId ? "/home" : "/new-user";
   return (
     <main className={styles.container}>
       <h1 className={styles.title}>Baseline</h1>
       <p>The Court is Calling</p>
       <p>–</p>
       <p>Track, Compete, Excel.</p>
-      <Link href="/register">
+      <Link href={href}>
         <Button className={styles.getStarted}>Get Started</Button>
       </Link>
     </main>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import Links from "./Links/Links";
 import styles from "./Navbar.module.css";
 import { auth } from "@clerk/nextjs";
 
-const Navbar = () => {
-  const { userId } = auth();
+const Navbar = async () => {
+  const { userId } = await auth();
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.logo}>Baseline</h1>
+        <Link href="/">
+          <h1 className={styles.logo}>Baseline</h1>
+        </Link>
         <Links userId={userId} />
       </div>
       <div className={styles.mark}></div>
