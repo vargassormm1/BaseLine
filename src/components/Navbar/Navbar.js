5dfb70cc59486a162dc881f148386cd747d20375
Ralph Vargas <ralphvargassorm@gmail.com>
2024-06-22 16:59:08 -0400

    added count badge in pendings tab

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,16 +2,29 @@ import Link from "next/link";
 import Links from "./Links/Links";
 import styles from "./Navbar.module.css";
 import { auth } from "@clerk/nextjs/server";
+import prisma from "@/utils/db";
+
+const getCurrentUser = async (clerkId) => {
+  if (!clerkId) return null;
+
+  const user = await prisma.user.findUnique({
+    where: { clerkId },
+    select: { userId: true, username: true },
+  });
+  return user;
+};
 
 const Navbar = async () => {
   const { userId } = auth();
+  const currentUser = await getCurrentUser(userId);
+
   return (
     <div className={styles.main}>
       <div className={styles.container}>
         <Link href="/">
           <h1 className={styles.logo}>Baseline</h1>
         </Link>
-        <Links userId={userId} />
+        <Links userId={userId} currentUser={currentUser} />
       </div>
       <div className={styles.mark}></div>
     </div>
