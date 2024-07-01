d4783b3df657c9daa8b3df2b10bedffd11f8b69d
Ralph Vargas <ralphvargassorm@gmail.com>
2024-07-01 13:30:59 -0400

    increased rate limit

--- src/app/api/pending/count/[userId]/route.js
+++ src/app/api/pending/count/[userId]/route.js
@@ -5,7 +5,7 @@ import { Redis } from "@upstash/redis";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(50, "30 s"),
+  limiter: Ratelimit.slidingWindow(100, "30 s"),
   analytics: true,
   prefix: "@upstash/ratelimit",
 });
