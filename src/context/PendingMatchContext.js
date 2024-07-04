fbd5aa6d545e3dd8db8a82ef1a22d05777968352
Ralph Vargas <ralphvargassorm@gmail.com>
2024-07-04 00:56:15 -0400

    pending matches now update through websockets

--- src/context/PendingMatchContext.js
+++ src/context/PendingMatchContext.js
@@ -5,12 +5,15 @@ const PendingMatchContext = createContext();
 
 const PendingMatchProvider = ({ children }) => {
   const [pendingMatchChanged, setPendingMatchChanged] = useState(false);
+  const [unreadMessageCount, setUnreadMessageCount] = useState(0);
 
   return (
     <PendingMatchContext.Provider
       value={{
         pendingMatchChanged,
         setPendingMatchChanged,
+        unreadMessageCount,
+        setUnreadMessageCount,
       }}
     >
       {children}
