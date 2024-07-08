d0a20fdb9994cdb1a66808b754b2827305218cf1
Ralph Vargas <ralphvargassorm@gmail.com>
2024-07-08 11:48:58 -0400

    added vercels speed insight

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,6 +3,8 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar/Navbar";
 import { PendingMatchProvider } from "../context/PendingMatchContext";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -19,6 +21,7 @@ export default function RootLayout({ children }) {
             <PendingMatchProvider>
               <Navbar />
               {children}
+              <SpeedInsights />
             </PendingMatchProvider>
           </div>
         </body>
