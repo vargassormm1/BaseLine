94477907bc417facbd9153223772b96217503517
Ralph Sormillon <ralphvargassorm@gmail.com>
2025-01-28 20:50:36 -0500

    Rankings table year filter

--- src/components/RankingsTable/RankingsTable.jsx
+++ src/components/RankingsTable/RankingsTable.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Image from "next/image";
 import styles from "./RankingsTable.module.css";
 import { getRankings } from "../../utils/api";
@@ -9,23 +9,42 @@ import Link from "next/link";
 const RankingsTable = () => {
   const [rankings, setRankings] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+  const getUserRankings = useCallback(async (year) => {
+    try {
+      const response = await getRankings(year); 
+      setRankings(response);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
 
   useEffect(() => {
-    const getUserRankings = async () => {
-      try {
-        const response = await getRankings();
-        setRankings(response);
-      } catch (error) {
-        console.log(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    getUserRankings();
-  }, []);
+    getUserRankings(selectedYear); 
+  }, [getUserRankings, selectedYear]);
 
   return (
     <div className={styles.rankingTable}>
+      <div className={styles.filterContainer}>
+        <select
+          id="yearFilter"
+          value={selectedYear}
+          onChange={(e) => setSelectedYear(e.target.value)}
+        >
+          <option value="all">All Time</option>
+          {Array.from(
+            { length: new Date().getFullYear() - 2024 + 1 },
+            (_, i) => 2024 + i
+          ).map((year) => (
+            <option key={year} value={year}>
+              {year}
+            </option>
+          ))}
+        </select>
+      </div>
       {loading ? (
         <div className={styles.spinnerContainer}>
           <Spinner />
@@ -43,32 +62,40 @@ const RankingsTable = () => {
             </tr>
           </thead>
           <tbody>
-            {rankings?.map((ranking) => (
-              <tr key={ranking.rank}>
-                <td style={{ textAlign: "center" }}>{ranking.rank}</td>
-                <td style={{ textAlign: "center" }}>
-                  <Link href={`/profile/${ranking.userId}`}>
-                    <Image
-                      className={styles.profilepic}
-                      src={ranking.imageUrl}
-                      width={50}
-                      height={50}
-                      alt="Picture of the user"
-                    />
-                  </Link>
-                </td>
-                <td style={{ textAlign: "center" }}>
-                  <Link href={`/profile/${ranking.userId}`}>
-                    {ranking.username}
-                  </Link>
-                </td>
-                <td style={{ textAlign: "center" }}>{ranking.totalWins}</td>
-                <td style={{ textAlign: "center" }}>{ranking.totalLosses}</td>
-                <td style={{ textAlign: "center" }}>
-                  <b>{ranking.totalPoints}</b>
+            {rankings.length === 0 ? (
+              <tr>
+                <td colSpan="6" style={{ textAlign: "center", padding: "20px" }}>
+                  Still waiting for the first game of the year!
                 </td>
               </tr>
-            ))}
+            ) : (
+              rankings?.map((ranking) => (
+                <tr key={ranking.rank}>
+                  <td style={{ textAlign: "center" }}>{ranking.rank}</td>
+                  <td style={{ textAlign: "center" }}>
+                    <Link href={`/profile/${ranking.userId}`}>
+                      <Image
+                        className={styles.profilepic}
+                        src={ranking.imageUrl}
+                        width={50}
+                        height={50}
+                        alt="Picture of the user"
+                      />
+                    </Link>
+                  </td>
+                  <td style={{ textAlign: "center" }}>
+                    <Link href={`/profile/${ranking.userId}`}>
+                      {ranking.username}
+                    </Link>
+                  </td>
+                  <td style={{ textAlign: "center" }}>{ranking.totalWins}</td>
+                  <td style={{ textAlign: "center" }}>{ranking.totalLosses}</td>
+                  <td style={{ textAlign: "center" }}>
+                    <b>{ranking.totalPoints}</b>
+                  </td>
+                </tr>
+              ))
+            )}
           </tbody>
         </table>
       )}
